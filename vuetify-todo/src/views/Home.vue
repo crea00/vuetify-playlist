2c07ba07feca0cea9dc54d0ed51e552f3e108d3a
Jerome <654321su@gmail.com>
2019-01-18 18:30:31 +0900

    Add button example

--- vuetify-todo/src/views/Home.vue
+++ vuetify-todo/src/views/Home.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo atque praesentium adipisci maxime voluptas! Aut, exercitationem. Iusto dolorem inventore ipsam minus, numquam, temporibus eaque et commodi officia vero ab. Ad?</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nihil autem animi impedit cum pariatur voluptas, molestiae neque perspiciatis enim, delectus quidem, cupiditate non numquam dolorum! Voluptates incidunt fugit voluptate.</p>
 
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller display</h4>
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed color="pink">Click me</v-btn>
+    <v-btn flat color="pink">Click me</v-btn>
 
-    <p class="headline">This is a headline</p>
-    <p class="subheading font-weight-bold">This is a subheading</p>
-    <p class="caption">this is a caption</p>
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>Email me</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
 <script>
-  
 export default {
-  
+
 }
 </script>
