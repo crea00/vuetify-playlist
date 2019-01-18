99b8578a95fd790e12a79223d5266e665509ab71
Jerome <654321su@gmail.com>
2019-01-18 18:38:00 +0900

    Add the example of Breakpoints & Visibility

--- vuetify-todo/src/views/Home.vue
+++ vuetify-todo/src/views/Home.vue
@@ -2,28 +2,11 @@
   <div class="home">
     <h1>Homepage</h1>
 
-    <v-btn class="pink white--text">Click me</v-btn>
-    <v-btn depressed color="pink">Click me</v-btn>
-    <v-btn flat color="pink">Click me</v-btn>
+    <!-- <v-btn class="hidden-md-and-down">Click me</v-btn> -->
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>Email me</span>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-up">Click me</v-btn> -->
 
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>Email me</span>
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">
-      <span>Email me</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
+    <v-btn class="hidden-sm-only">Click me</v-btn>
 
   </div>
 </template>
