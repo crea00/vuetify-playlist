914436d54ae615ac1c4ad516af42e7b3f9b22b70
Jerome <654321su@gmail.com>
2019-01-19 19:10:01 +0900

    Create a Navbar Component

--- vuetify-todo/src/App.vue
+++ vuetify-todo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- v-app is the Root Component we use in from Vuetify -->
-  <v-app>
-
+  <v-app class="grey lighten-4">
+    <Navbar />
   
     <v-content>
       <router-view></router-view>
@@ -11,9 +11,13 @@
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 
 export default {
   name: 'App',
+  components: { 
+    Navbar
+  },
   data () {
     return {
       //
--- vuetify-todo/src/views/Home.vue
+++ vuetify-todo/src/views/Home.vue
@@ -2,12 +2,6 @@
   <div class="home">
     <h1>Homepage</h1>
 
-    <!-- <v-btn class="hidden-md-and-down">Click me</v-btn> -->
-
-    <!-- <v-btn class="hidden-md-and-up">Click me</v-btn> -->
-
-    <v-btn class="hidden-sm-only">Click me</v-btn>
-
   </div>
 </template>
 
