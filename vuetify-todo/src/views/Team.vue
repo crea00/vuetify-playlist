7571508f9bf04a74ecef8e4e506918c9ad56aa46
Jerome <654321su@gmail.com>
2019-01-20 18:54:39 +0900

    Add the Card Components of team

--- vuetify-todo/src/views/Team.vue
+++ vuetify-todo/src/views/Team.vue
@@ -3,7 +3,27 @@
     <h1 class="subheading grey--text">Team</h1>
 
     <v-container class="my-5">
-      content
+      
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+              image here
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+              <v-card-actions>
+                <v-btn flat color="grey">
+                  <v-icon small left>message</v-icon>
+                  <span>Message</span>
+                </v-btn>
+              </v-card-actions>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
     </v-container>
 
   </div>
@@ -11,6 +31,17 @@
 
 <script>
 export default {
-  name: 'Team'
+  name: 'Team',
+  data() {
+    return {
+      team: [
+        { name: 'Jerome', role: 'Web developer' },
+        { name: 'Ryu', role: 'Graphic designer' },
+        { name: 'Chun Li', role: 'Web developer' },
+        { name: 'Gouken', role: 'Social media maverick' },
+        { name: 'Yoshi', role: 'Sales guru'  }
+      ]
+    }  
+  }
 }
 </script>
