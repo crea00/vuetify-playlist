163a283789e952864dd67d4e5c0f2e7f3b4a698c
Jerome <654321su@gmail.com>
2019-01-22 11:32:32 +0900

    Install Firebase & Add Firestore Data
     - Add a dependency: firebase
     - Add a Devdependency: dotenv

--- vuetify-todo/src/components/Popup.vue
+++ vuetify-todo/src/components/Popup.vue
@@ -27,6 +27,7 @@
 
 <script>
 import format from 'date-fns/format'
+import db from '@/fb';
 
 export default {
   name: 'Popup',
@@ -43,7 +44,17 @@ export default {
   methods: {
     submit() {
       if(this.$refs.form.validate()) {
-        console.log(this.title, this.content)
+        const project = {
+          title: this.title,
+          content: this.content,
+          due: format(this.due, 'Do MMM YYYY'),
+          person: 'Jerome',
+          status:  'ongoing'
+        }
+
+        db.collection('projects').add(project).then(() => {
+          console.log('added to db');
+        })
       }
     }
   },
